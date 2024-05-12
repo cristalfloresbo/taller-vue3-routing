1b1974f949a27eb9ff90ed513fd79d36f19a883c
Cristal Flores <cristalflores.bo@gmail.com>
2024-05-12 17:18:24 -0400

    Set up 404 Not Found Route

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import AppHome from "./components/AppHome.vue";
 import AppAbout from "./components/AppAbout.vue";
+import NotFoundComponent from "./components/NotFoundComponent.vue"
 
 import { createWebHashHistory, createRouter } from "vue-router";
 
 const routes = [
   { path: "/", component: AppHome },
   { path: "/about", component: AppAbout },
+  { path: "/:pathMatch(.*)", component: NotFoundComponent },
 ];
 
 const router = createRouter({
