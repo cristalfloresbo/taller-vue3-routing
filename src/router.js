dbf4afdb3861bd2e0de2d77fa829a0df9af9b283
Cristal Flores <cristalflores.bo@gmail.com>
2024-05-12 18:16:41 -0400

    Navigation Guards

--- src/router.js
+++ src/router.js
@@ -1,14 +1,23 @@
 import AppHome from "./components/AppHome.vue";
 import AppAbout from "./components/AppAbout.vue";
 import AppUser from "./components/AppUser.vue";
-import NotFoundComponent from "./components/NotFoundComponent.vue"
+import NotFoundComponent from "./components/NotFoundComponent.vue";
 
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: "/", name: 'home', component: AppHome },
-  { path: "/about", name: 'about', component: AppAbout },
-  { path: "/user/:id", name: 'user', component: AppUser },
+  { path: "/", name: "home", component: AppHome },
+  { path: "/about", name: "about", component: AppAbout },
+  {
+    path: "/user/:id",
+    name: "user",
+    component: AppUser,
+    beforeEnter: (to, from, next) => {
+      console.log("beforeEnter", "to", to);
+      console.log("beforeEnter", "from", from);
+      next();
+    },
+  },
   { path: "/:pathMatch(.*)", component: NotFoundComponent },
 ];
 /*
@@ -24,8 +33,20 @@ Requires server configuration to ensure that the server serves the same page for
 \
 */
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
+
+//Guards
+router.beforeEach((to, from, next) => {
+  console.log("beforeEach", "to", to);
+  console.log("beforeEach", "from", from);
+  next();
+});
+
+router.addRoute((to, from) => {
+  console.log("afterEach", "to", to);
+  console.log("afterEach", "from", from);
+});
 
 export default router;
